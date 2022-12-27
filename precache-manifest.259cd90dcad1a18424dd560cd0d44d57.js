65e83f4f1e6917e02a9c1c09fecfb54cbe76d876
mohmedsajid <sajju123@live.in>
2022-12-27 13:19:58 -0500

    Updates

---
+++ precache-manifest.259cd90dcad1a18424dd560cd0d44d57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a0ece68d65bf817d3fd",
+    "url": "./static/css/main.6d0d8026.chunk.css"
+  },
+  {
+    "revision": "5a0ece68d65bf817d3fd",
+    "url": "./static/js/main.63a89aa1.chunk.js"
+  },
+  {
+    "revision": "1352b8877773e02c55ce",
+    "url": "./static/js/runtime~main.d673871a.js"
+  },
+  {
+    "revision": "a7073e04a979543ce30b",
+    "url": "./static/js/2.bbc4abf0.chunk.js"
+  },
+  {
+    "revision": "8a02dddbcdc9deb302ba",
+    "url": "./static/js/3.bf5fdb0a.chunk.js"
+  },
+  {
+    "revision": "fac9aec1af94309fb438b308547d9fd1",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pizzamakers/precache-manifest.95f34be5bf2027cca22b5f3f9afb3f08.js"
+  "./precache-manifest.259cd90dcad1a18424dd560cd0d44d57.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/pizzamakers/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
