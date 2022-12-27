94edacbd1f9d84156dab8f39b1856f47578e19ef
mohmedsajid <sajju123@live.in>
2022-12-27 16:58:15 -0500

    images

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 export default function Navbar(props){
     return( 
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark text-light brand">
-      <Link to="/" className='brand logo'>PizzaMakers</Link>
+      <Link to="/pizzamakers" className='brand logo'>PizzaMakers</Link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -12,7 +12,7 @@ export default function Navbar(props){
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
-            <Link to="/">Home</Link>
+            <Link to="/pizzamakers">Home</Link>
           </li>
           <li className="nav-item">
             <Link to="/menu">Menu</Link>
