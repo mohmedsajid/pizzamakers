23bb6778cae86b905b8c74101b517884b6a3831f
mohmedsajid <sajju123@live.in>
2022-12-27 11:46:08 -0500

    Updates

---
+++ precache-manifest.95f34be5bf2027cca22b5f3f9afb3f08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a0ece68d65bf817d3fd",
+    "url": "/pizzamakers/static/css/main.6d0d8026.chunk.css"
+  },
+  {
+    "revision": "5a0ece68d65bf817d3fd",
+    "url": "/pizzamakers/static/js/main.63a89aa1.chunk.js"
+  },
+  {
+    "revision": "de97ea96192cabe595c4",
+    "url": "/pizzamakers/static/js/runtime~main.951cd5ab.js"
+  },
+  {
+    "revision": "a7073e04a979543ce30b",
+    "url": "/pizzamakers/static/js/2.bbc4abf0.chunk.js"
+  },
+  {
+    "revision": "8a02dddbcdc9deb302ba",
+    "url": "/pizzamakers/static/js/3.bf5fdb0a.chunk.js"
+  },
+  {
+    "revision": "993462894d2582acdec1569d8d6246f2",
+    "url": "/pizzamakers/index.html"
+  }
+];
