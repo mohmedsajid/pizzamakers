91255cdcf984ec9dc776f2780c308b8c54a3324a
mohmedsajid <sajju123@live.in>
2022-12-27 15:42:17 -0500

    link

--- src/App.js
+++ src/App.js
@@ -14,8 +14,7 @@ function App() {
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <NavbarTrends/>
         <Routes>
-          <Route path="/" element={<Navigate to="/pizzamakers" />} /> 
-          <Route path="/pizzamakers" element={<Home/>}></Route>
+          <Route path="/" element={<Home/>}></Route>
           <Route path="/about-us" element={<AboutUs/>}></Route>
           <Route path="/contact-us" element={<ContactUs/>}></Route>
           <Route path="/menu" element={<Menu/>}></Route>
